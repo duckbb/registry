35ba66fb037f9fed77220b3a47839e4f5a6c38ca
duckbb <3044098503@qq.com>
2020-12-28 20:39:28 +0800

    #add nacos registry

--- registry.go
+++ registry.go
@@ -5,24 +5,51 @@ import "context"
 //plunin interface
 type Registryer interface {
 	Name() string
-	Init(context.Context) error
+	Init(context.Context, func() error) error
 	Register(context.Context, *Service) error
 	UnRegister(context.Context, *Service) error
 	Get(context.Context, *Service) error
 }
 
-//service
 type Service struct {
-	Name  string  `json:"name"`
-	nodes []*Node `json:"nodes"`
+
+	//nacos init
+	NacosIp          string
+	NacosPort        uint64
+	NacosWeight      float64
+	NacosEnable      bool
+	NacosHealthy     bool
+	NacosMetadata    map[string]string
+	NacosClusterName string
+	NacosServiceName string
+	NacosGroupName   string
+	NacosEphemeral   bool
 }
 
-// node
-type Node struct {
-	Addr   string `json:"addr"`
-	Group  string `json:"group"` //label for group Service
-	Region string `json:"region"`
-	Zone   string `json:"zone"`
-
-	Weight int `json:"weight"` // weight round roubin
-}
+////service
+//type Service struct {
+//	Name  string  `json:"name"`
+//	nodes []*Node `json:"nodes"`
+//}
+//
+//// node
+//type Node struct {
+//	Ip       string `json:"ip"`
+//	Port     string `json:"port"`
+//	Group    string `json:"group"` //label for group Service
+//	Region   string `json:"region"`
+//	Zone     string `json:"zone"`
+//	Metadata map[string]string
+//	Weight   int `json:"weight"` // weight round roubin
+//
+//	Ip:          "10.0.0.11",
+//	Port:        8848,
+//	ServiceName: "demo.go",
+//	Weight:      10,
+//	Enable:      true,
+//	Healthy:     true,
+//	Ephemeral:   true,
+//	Metadata:    map[string]string{"idc":"shanghai"},
+//	ClusterName: "cluster-a", // 默认值DEFAULT
+//	GroupName:   "group-a",  // 默认值DEFAULT_GROUP
+//}
