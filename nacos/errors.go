c226b62f5a892272857508574bac7de3b629f496
duckbb <3044098503@qq.com>
2020-12-29 12:00:09 +0800

    #add nacos registry set get

--- nacos/errors.go
+++ nacos/errors.go
@@ -4,8 +4,9 @@ import "errors"
 
 var NacosNotFoundErr = errors.New("The client could not be found")
 var NacosWeightErr = errors.New("Weight must be lager than 0")
-var NacosRegistionErr = errors.New("registion failed")
-var NacosParamErr = errors.New("registion failed")
-var NacosDeregistionErr = errors.New("deregistion failed")
+var NacosRegistionErr = errors.New("Registion failed")
+var NacosParamErr = errors.New("Registion failed")
+var NacosDeregistionErr = errors.New("Deregistion failed")
+var NacosGetServiceErr = errors.New("Nacos get services failed")
 
 //DeregisterInstance
--- nacos/nacos.go
+++ nacos/nacos.go
@@ -3,6 +3,9 @@ package nacos
 import (
 	"context"
 	"fmt"
+	"sync"
+
+	"github.com/nacos-group/nacos-sdk-go/vo"
 
 	"github.com/nacos-group/nacos-sdk-go/clients"
 
@@ -15,9 +18,10 @@ import (
 
 const RegistryName = "nacos"
 
-var Client naming_client.INamingClient
-
 type NacosRegistry struct {
+	lock     sync.Mutex
+	Client   naming_client.INamingClient
+	Services map[string][]*registry.Service
 }
 
 func (n *NacosRegistry) PluginName() string {
@@ -25,12 +29,19 @@ func (n *NacosRegistry) PluginName() string {
 }
 
 //init nacos client
-func NacosInit(ClientConfig *constant.ClientConfig, ServerConfig []constant.ServerConfig) (err error) {
-	Client, err = clients.CreateNamingClient(map[string]interface{}{
-		"serverConfigs": ClientConfig,
-		"clientConfig":  ServerConfig,
+func NacosInit(ClientConfig constant.ClientConfig, ServerConfig []constant.ServerConfig) (*NacosRegistry, error) {
+	client, err := clients.CreateNamingClient(map[string]interface{}{
+		"clientConfig":  ClientConfig,
+		"serverConfigs": ServerConfig,
 	})
-	return err
+	if err != nil {
+		return nil, err
+	}
+	r := &NacosRegistry{
+		Client:   client,
+		Services: map[string][]*registry.Service{},
+	}
+	return r, err
 }
 
 func (n *NacosRegistry) Init(ctx context.Context, f func() error) error {
@@ -39,7 +50,7 @@ func (n *NacosRegistry) Init(ctx context.Context, f func() error) error {
 
 //register service
 func (n *NacosRegistry) Register(ctx context.Context, service *registry.Service) error {
-	if Client == nil {
+	if n.Client == nil {
 		return NacosNotFoundErr
 	}
 	resisterParam, err := NewRegisterInstanceParam(service)
@@ -47,7 +58,7 @@ func (n *NacosRegistry) Register(ctx context.Context, service *registry.Service)
 		return err
 	}
 
-	success, err := Client.RegisterInstance(*resisterParam)
+	success, err := n.Client.RegisterInstance(*resisterParam)
 	if err != nil {
 		return fmt.Errorf("%w,source Err:%s", NacosRegistionErr, err)
 	}
@@ -59,12 +70,12 @@ func (n *NacosRegistry) Register(ctx context.Context, service *registry.Service)
 
 //unregister service
 func (n *NacosRegistry) UnRegister(ctx context.Context, service *registry.Service) error {
-	if Client == nil {
+	if n.Client == nil {
 		return NacosNotFoundErr
 	}
 	resisterParam := NewDeregisterInstanceParam(service)
 
-	success, err := Client.DeregisterInstance(*resisterParam)
+	success, err := n.Client.DeregisterInstance(*resisterParam)
 	if err != nil {
 		return fmt.Errorf("%w,source Err:%s", NacosDeregistionErr, err)
 	}
@@ -74,18 +85,97 @@ func (n *NacosRegistry) UnRegister(ctx context.Context, service *registry.Servic
 	return nil
 }
 
-func (n *NacosRegistry) Get(ctx context.Context, service *registry.Service) error {
-	if Client == nil {
-		return NacosNotFoundErr
+//get services
+func (n *NacosRegistry) Get(ctx context.Context, service *registry.Service) ([]*registry.Service, error) {
+	if srvs, ok := n.Services[service.NacosServiceName]; ok {
+		return srvs, nil
 	}
-	resisterParam := NewDeregisterInstanceParam(service)
-
-	success, err := Client.DeregisterInstance(*resisterParam)
+	if n.Client == nil {
+		return nil, NacosNotFoundErr
+	}
+	n.lock.Lock()
+	defer n.lock.Unlock()
+	//service healthy=true,enable=true 和weight>0
+	param := NewSelectInstances(service)
+	instances, err := n.Client.SelectInstances(*param)
 	if err != nil {
-		return fmt.Errorf("%w,source Err:%s", NacosDeregistionErr, err)
+		return nil, fmt.Errorf("%w,source Err:%s", NacosGetServiceErr, err)
 	}
-	if !success {
-		return NacosDeregistionErr
+	srvs := []*registry.Service{}
+	for _, v := range instances {
+		srv := &registry.Service{
+			NacosIp:          v.Ip,
+			NacosPort:        v.Port,
+			NacosWeight:      v.Weight,
+			NacosHealthy:     v.Healthy,
+			NacosMetadata:    v.Metadata,
+			NacosClusterName: v.ClusterName,
+			NacosServiceName: v.ServiceName,
+			NacosEphemeral:   false,
+		}
+		srvs = append(srvs, srv)
 	}
-	return nil
+	n.Services[service.NacosServiceName] = srvs
+	return srvs, nil
+
+}
+
+func NewNacosService(ServiceName, Ip string, Port uint64, Weight float64, Enable, Healthy bool, opts ...NacosServiceOption) *registry.Service {
+	srv := &registry.Service{
+		NacosIp:          Ip,
+		NacosPort:        Port,
+		NacosWeight:      Weight,
+		NacosEnable:      Enable,
+		NacosHealthy:     Healthy,
+		NacosServiceName: ServiceName,
+	}
+	for _, opt := range opts {
+		opt(srv)
+	}
+	return srv
+}
+
+func NewRegisterInstanceParam(srv *registry.Service) (*vo.RegisterInstanceParam, error) {
+	c := &vo.RegisterInstanceParam{
+		Ip:          srv.NacosIp,
+		Port:        srv.NacosPort,
+		Weight:      srv.NacosWeight,
+		Enable:      srv.NacosEnable,
+		Healthy:     srv.NacosHealthy,
+		Metadata:    srv.NacosMetadata,
+		ClusterName: srv.NacosClusterName,
+		ServiceName: srv.NacosServiceName,
+		GroupName:   srv.NacosGroupName,
+		Ephemeral:   srv.NacosEphemeral,
+	}
+	if c.Weight <= 0 {
+		return nil, NacosWeightErr
+	}
+	return c, nil
+}
+
+func NewDeregisterInstanceParam(srv *registry.Service) *vo.DeregisterInstanceParam {
+	c := &vo.DeregisterInstanceParam{
+		Ip:          srv.NacosIp,
+		Port:        srv.NacosPort,
+		Cluster:     srv.NacosClusterName,
+		ServiceName: srv.NacosServiceName,
+		GroupName:   srv.NacosGroupName,
+		Ephemeral:   srv.NacosEphemeral,
+	}
+	return c
+}
+
+func NewSelectInstances(srv *registry.Service) *vo.SelectInstancesParam {
+	c := &vo.SelectInstancesParam{
+		ServiceName: srv.NacosServiceName,
+		HealthyOnly: true,
+	}
+	if srv.NacosClusterName != "" {
+		c.Clusters = []string{srv.NacosClusterName}
+	}
+	if srv.NacosGroupName != "" {
+		c.GroupName = srv.NacosGroupName
+	}
+	return c
 }
