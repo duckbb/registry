75169147f75ac7254f2a419436b0443d6bb15bea
duckbb <3044098503@qq.com>
2020-12-30 19:05:47 +0800

    #add nacos registry test

--- registry_mgr.go
+++ registry_mgr.go
@@ -1,48 +1,78 @@
 package registry
 
-//
-//import (
-//	"context"
-//	"fmt"
-//	"sync"
-//)
-//
-//type pluninRegistry struct {
-//	sync.Mutex
-//	plunins map[string]Registryer
-//}
-//
-//var PR *pluninRegistry
-//var once sync.Once
-//
-//func GetRegistryMgr() *pluninRegistry {
-//	once.Do(func() {
-//		PR = &pluninRegistry{plunins: make(map[string]Registryer)}
-//	})
-//	return PR
-//}
-//
-////plunin regisrer
-//func (p *pluninRegistry) RegisterPlunin(r Registryer) error {
-//	p.Lock()
-//	defer p.Unlock()
-//	if _, ok := p.plunins[r.Name()]; ok {
-//		return fmt.Errorf("%s had register", r.Name())
-//	}
-//	p.plunins[r.Name()] = r
-//	return nil
-//}
-//
-////plunin init
-//func (p *pluninRegistry) InitPluninRegistry(ctx context.Context, name string, opts ...Option) error {
-//	p.Lock()
-//	defer p.Unlock()
-//	if _, ok := p.plunins[name]; ok {
-//		return fmt.Errorf("%s plun-in not register", name)
-//	}
-//	err := p.plunins[name].Init(ctx, opts...)
-//	if err != nil {
-//		return fmt.Errorf("plun-in init fail,err:%s", err)
-//	}
-//	return nil
-//}
+import (
+	"context"
+	"fmt"
+	"sync"
+)
+
+type RegistryType string
+
+const (
+	NACOS     RegistryType = "nacos"
+	ZOOKEEPER RegistryType = "zookeeper"
+)
+
+type pluninRegistry struct {
+	sync.Mutex
+	plunins             map[RegistryType]Registryer
+	currentRegisterType RegistryType
+}
+
+var PR *pluninRegistry
+var once sync.Once
+
+func GetRegistryMgr() *pluninRegistry {
+	once.Do(func() {
+		PR = &pluninRegistry{plunins: make(map[RegistryType]Registryer)}
+	})
+	return PR
+}
+
+//set current RegisterType
+func (p *pluninRegistry) SetRegisterType(name RegistryType) {
+	p.Lock()
+	defer p.Unlock()
+	p.currentRegisterType = name
+}
+
+//plunin init
+func (p *pluninRegistry) InitPluninRegistry(ctx context.Context, name RegistryType, r Registryer) error {
+	p.Lock()
+	defer p.Unlock()
+	if _, ok := p.plunins[name]; ok {
+		return fmt.Errorf("%s plun-in has registered", name)
+	}
+	p.plunins[name] = r
+	return nil
+}
+
+//register service
+func (p *pluninRegistry) Register(ctx context.Context, service *Service, name ...RegistryType) error {
+	p.Lock()
+	defer p.Unlock()
+	var rType RegistryType = p.currentRegisterType
+	if len(name) > 0 {
+		rType = name[0]
+	}
+	r, ok := p.plunins[rType]
+	if ok {
+		return fmt.Errorf("%s plun-in has registered", name)
+	}
+	return r.Register(ctx, service)
+}
+
+//unregister service
+func (p *pluninRegistry) UnRegister(ctx context.Context, service *Service, name ...RegistryType) error {
+	p.Lock()
+	defer p.Unlock()
+	var rType RegistryType = p.currentRegisterType
+	if len(name) > 0 {
+		rType = name[0]
+	}
+	r, ok := p.plunins[rType]
+	if ok {
+		return fmt.Errorf("%s plun-in has registered", name)
+	}
+	return r.UnRegister(ctx, service)
+}
